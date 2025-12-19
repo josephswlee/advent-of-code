be912b1c4a5a95d9fb33a460826cb55451d84c33
Joseph Lee <eniac@mac.mynetworksettings.com>
2025-12-18 22:51:06 -0500

    feat: day 7 part 1 of advent of code

--- 2025/day6/day6-trash-compactor.py
+++ 2025/day6/day6-trash-compactor.py
@@ -8,7 +8,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Pass in the banks text file")
 
-parser.add_argument("--file", type=str, help="input.txt for the day 4 of advent of code")
+parser.add_argument("--file", type=str, help="input.txt for the day 6 of advent of code")
 
 args = parser.parse_args()
 
